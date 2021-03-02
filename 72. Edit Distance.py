f5853a8fc76c80809c88e87429a1deaf6ba8d7fd
Katherinaxxx <359391236@qq.com>
2021-03-02 19:35:56 +0800

    1

--- 72. Edit Distance.py
+++ 72. Edit Distance.py
@@ -29,7 +29,7 @@ class Solution(object):
                 if word1[i-1] == word2[j-1]:
                     dp[i][j] = dp[i-1][j-1]
                 else:
-                    # 增、删、
+                    # 增、删、改
                     dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
         #print(dp)
         return dp[-1][-1]
